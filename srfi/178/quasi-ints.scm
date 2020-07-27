4dd7cf3ee9339f75e855fdcb943f2bed12220ab7
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-07-26 23:30:26 -0400

    Add quasi-integer functions.

--- srfi/178/quasi-ints.scm
+++ srfi/178/quasi-ints.scm
@@ -5,11 +5,21 @@
   #f)
 
 (define (bitvector-count bit bvec)
-  #f)
+  (bitvector-fold/int (lambda (n b) (if (= b bit) (+ n 1) n))
+		      0
+		      bvec))
 
 (define (bitvector-if if-bvec then-bvec else-bvec)
-  #f)
+  (bitvector-map/bool (lambda (bit then-bit else-bit)
+			(if bit then-bit else-bit))
+		      if-bvec
+		      then-bvec
+		      else-bvec))
 
 (define (bitvector-first-bit bit bvec)
-  #f)
+  (let ((len (bitvector-length bvec)))
+    (let lp ((i 0))
+      (cond ((>= i len) -1)
+	    ((= bit (bitvector-ref/int bvec i)) i)
+	    (else (lp (+ i 1)))))))
 
