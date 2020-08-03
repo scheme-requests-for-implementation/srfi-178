1fa0c11f68aa385296d658e03ce8c77b2e1af3a3
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-08-03 18:14:20 -0400

    Add tests for remaining logical ops.

--- test/logic-ops.scm
+++ test/logic-ops.scm
@@ -1,39 +1,122 @@
-(define (%integer->bitvector-byte int)
-  (bitvector-pad-right 0 (integer->bitvector int) 8))
-
 (define (check-bitwise-operations)
-  (define test-int1 #xb5)
-  (define test-int2 #xca)
-  (define test-int3 #xfe)
-  (define test-bvec1 (%integer->bitvector-byte test-int1))
-  (define test-bvec2 (%integer->bitvector-byte test-int2))
-  (define test-bvec3 (%integer->bitvector-byte test-int3))
+  (define test-bvec1 (bitvector 1 0 1 0))
+  (define test-bvec2 (bitvector 1 1 0 0))
+  (define test-bvec3 (bitvector 0 0 1 1))
   (print-header "Checking bitwise operations...")
 
   ;;; not
 
-  (check (bitvector= (bitvector-not test-bvec1)
-                     (bitvector 0 1 0 1 0 0 1 0))
+  (check (bitvector= (bitvector-not test-bvec1) (bitvector 0 1 0 1))
    => #t)
-  (check (bitvector= (bitvector-not (bitvector-not test-bvec1)) test-bvec1)
+  (check (bitvector= (bitvector-not (bitvector-not test-bvec1))
+                     test-bvec1)
    => #t)
 
-  ;;; Associative binary operations.
+  ;;; Associative operations
 
-  (check (bitvector=
-          (bitvector-and test-bvec1 test-bvec2)
-          (%integer->bitvector-byte (bitwise-and test-int1 test-int2)))
+  (check (bitvector= (bitvector-and test-bvec1 test-bvec2 test-bvec3)
+                     (bitvector 0 0 0 0))
    => #t)
-  (check (bitvector=
-          (bitvector-ior test-bvec1 test-bvec2)
-          (%integer->bitvector-byte (bitwise-ior test-int1 test-int2)))
+  (check (bitvector= (bitvector-ior test-bvec1 test-bvec2 test-bvec3)
+                     (bitvector 1 1 1 1))
    => #t)
-  (check (bitvector=
-          (bitvector-xor test-bvec1 test-bvec2)
-          (%integer->bitvector-byte (bitwise-xor test-int1 test-int2)))
+  (check (bitvector= (bitvector-xor test-bvec1 test-bvec2 test-bvec3)
+                     (bitvector 0 1 0 1))
    => #t)
-  (check (bitvector=
-          (bitvector-eqv test-bvec1 test-bvec2)
-          (%integer->bitvector-byte (bitwise-eqv test-int1 test-int2)))
+  (check (bitvector= (bitvector-eqv test-bvec1 test-bvec2 test-bvec3)
+                     (bitvector 0 1 0 1))
    => #t)
+
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-and! test-bvec1* test-bvec2 test-bvec3)
+                  test-bvec1*)
+                 (bitvector 0 0 0 0))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-ior! test-bvec1* test-bvec2 test-bvec3)
+                  test-bvec1*)
+                 (bitvector 1 1 1 1))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-xor! test-bvec1* test-bvec2 test-bvec3)
+                  test-bvec1*)
+                 (bitvector 0 1 0 1))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-eqv! test-bvec1* test-bvec2 test-bvec3)
+                  test-bvec1*)
+                 (bitvector 0 1 0 1))
+     => #t))
+
+  ;;; Non-associative binary operations
+
+  (check (bitvector= (bitvector-nand test-bvec1 test-bvec2)
+                     (bitvector 0 1 1 1))
+   => #t)
+  (check (bitvector= (bitvector-nor test-bvec1 test-bvec2)
+                     (bitvector 0 0 0 1))
+   => #t)
+  (check (bitvector= (bitvector-andc1 test-bvec1 test-bvec2)
+                     (bitvector 0 1 0 0))
+   => #t)
+  (check (bitvector= (bitvector-andc2 test-bvec1 test-bvec2)
+                     (bitvector 0 0 1 0))
+   => #t)
+  (check (bitvector= (bitvector-orc1 test-bvec1 test-bvec2)
+                     (bitvector 1 1 0 1))
+   => #t)
+  (check (bitvector= (bitvector-orc2 test-bvec1 test-bvec2)
+                     (bitvector 1 0 1 1))
+   => #t)
+
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-nand! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 0 1 1 1))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-nor! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 0 0 0 1))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-andc1! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 0 1 0 0))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-andc2! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 0 0 1 0))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-orc1! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 1 1 0 1))
+     => #t))
+  (let ((test-bvec1* (bitvector-copy test-bvec1)))
+    (check
+     (bitvector= (begin
+                  (bitvector-orc2! test-bvec1* test-bvec2)
+                  test-bvec1*)
+                 (bitvector 1 0 1 1))
+     => #t))
 )
