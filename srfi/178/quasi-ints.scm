00edb4a998c6b6bb835d25d73411bcdabfa63463
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-08-03 19:08:55 -0400

    Unwrap bit argument in -count and -first-bit.

--- srfi/178/quasi-ints.scm
+++ srfi/178/quasi-ints.scm
@@ -5,9 +5,10 @@
   #f)
 
 (define (bitvector-count bit bvec)
-  (bitvector-fold/int (lambda (n b) (if (= b bit) (+ n 1) n))
-		      0
-		      bvec))
+  (let ((int (I bit)))
+    (bitvector-fold/int (lambda (n b) (if (= b int) (+ n 1) n))
+                        0
+                        bvec)))
 
 (define (bitvector-if if-bvec then-bvec else-bvec)
   (bitvector-map/bool (lambda (bit then-bit else-bit)
@@ -17,9 +18,9 @@
 		      else-bvec))
 
 (define (bitvector-first-bit bit bvec)
-  (let ((len (bitvector-length bvec)))
+  (let ((int (I bit)) (len (bitvector-length bvec)))
     (let lp ((i 0))
       (cond ((>= i len) -1)
-	    ((= bit (bitvector-ref/int bvec i)) i)
-	    (else (lp (+ i 1)))))))
+            ((= int (bitvector-ref/int bvec i)) i)
+            (else (lp (+ i 1)))))))
 
