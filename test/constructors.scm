0cd2434b2c276194dadfa87b45b38ed2673e9943
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-08-21 00:47:31 -0400

    Tests: Improve constructor test style.
    Prefer testing with bitvector= over converting to lists and checking
    with equal?.

--- test/constructors.scm
+++ test/constructors.scm
@@ -1,9 +1,9 @@
 (define (check-constructors)
   (print-header "Checking constructors...")
 
-  (check (bitvector-length (make-bitvector 8))       => 8)
-  (check (bitvector->list/int (make-bitvector 4 0))  => '(0 0 0 0))
-  (check (bitvector->list/int (make-bitvector 4 #t)) => '(1 1 1 1))
+  (check (bitvector-length (make-bitvector 8))                  => 8)
+  (check (bitvector= (make-bitvector 4 0) (bitvector 0 0 0 0))  => #t)
+  (check (bitvector= (make-bitvector 4 #t) (bitvector 1 1 1 1)) => #t)
 
   ;;; unfolds
 
@@ -44,34 +44,42 @@
 
   (let ((bvec (bitvector 1 0 1 0)))
     (check (bitvector= bvec (bitvector-copy bvec)) => #t)
-    (check (eqv? bvec (bitvector-copy bvec)) => #f))  ; fresh copy
-  (check (bitvector->list/int (bitvector-copy (bitvector 1 0 1 0) 1))
-   => '(0 1 0))
-  (check (bitvector->list/int (bitvector-copy (bitvector 1 0 1 0) 2 4))
-   => '(1 0))
+    (check (eqv? bvec (bitvector-copy bvec)) => #f))  ; fresh copy?
+  (check (bitvector= (bitvector-copy (bitvector 1 0 1 0) 1) (bitvector 0 1 0))
+   => #t)
+  (check (bitvector= (bitvector-copy (bitvector 1 0 1 0) 2 4) (bitvector 1 0))
+   => #t)
 
   (let ((bvec (bitvector 1 0 1 0)))
-    (check (bitvector->list/int (bitvector-reverse-copy bvec))
-     => (reverse (bitvector->list/int bvec)))
-    (check (eqv? bvec (bitvector-reverse-copy bvec)) => #f))  ; fresh copy
-  (check (bitvector->list/int (bitvector-reverse-copy (bitvector 1 0 1 0) 1))
-   => '(0 1 0))
-  (check (bitvector->list/int (bitvector-reverse-copy (bitvector 1 0 1 0) 2 4))
-   => '(0 1))
+    (check (equal? (bitvector->list/int (bitvector-reverse-copy bvec))
+                   (reverse (bitvector->list/int bvec)))
+     => #t)
+    (check (eqv? bvec (bitvector-reverse-copy bvec)) => #f))  ; fresh copy?
+  (check (bitvector= (bitvector-reverse-copy (bitvector 1 0 1 0) 1)
+                     (bitvector 0 1 0))
+   => #t)
+  (check (bitvector= (bitvector-reverse-copy (bitvector 1 0 1 0) 2 4)
+                     (bitvector 0 1))
+   => #t)
 
   ;;; append & concatenate
 
-  (check (bitvector->list/int
-          (bitvector-append (bitvector 1 0) (bitvector 0 1)))
-   => '(1 0 0 1))
-  (check (bitvector->list/int
-          (bitvector-append (bitvector 1 0) (bitvector 0 1) (bitvector)))
-   => '(1 0 0 1))
-  (check (bitvector->list/int
+  (check (bitvector=
+          (bitvector-append (bitvector 1 0) (bitvector 0 1))
+          (bitvector 1 0 0 1))
+   => #t)
+  (check (bitvector=
+          (bitvector-append (bitvector 1 0) (bitvector 0 1) (bitvector))
+          (bitvector 1 0 0 1))
+   => #t)
+  (check (bitvector=
           (bitvector-concatenate
-           (list (bitvector 1 0) (bitvector 0 1) (bitvector))))
-   => '(1 0 0 1))
-  (check (bitvector->list/int
+           (list (bitvector 1 0) (bitvector 0 1) (bitvector)))
+          (bitvector 1 0 0 1))
+   => #t)
+  (check (bitvector=
           (bitvector-append-subbitvectors (bitvector 1 0 0 1) 0 2
-                                          (bitvector 1 1 1 1) 2 4))
-   => '(1 0 1 1)))
+                                          (bitvector 1 1 1 1) 2 4)
+          (bitvector 1 0 1 1))
+   => #t)
+)
